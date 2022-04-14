16f7401eac185e308a9632421cdd589c27784b20
mfbomb <102908916+mfbomb@users.noreply.github.com>
2022-04-14 10:26:17 +0900

    Update auto.py

--- auto.py
+++ auto.py
@@ -67,8 +67,8 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
         schedule.run_pending()
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.1)
+        if start_time < now < end_time + datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-BTC", 0.2)
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
